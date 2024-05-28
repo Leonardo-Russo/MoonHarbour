824dd9a88d5fa43fb357b6ea47e5fb8a7a15411f
Leonardo Russo <leonardo.rxsso@gmail.com>
2024-05-28 16:50:23 +0200

    housekeeping

--- Simulations/parsimulations.m
+++ Simulations/parsimulations.m
@@ -32,28 +32,28 @@ successful_vel_tol = 1e-2;      % 1 cm/s
 success_color = '#39e617';
 failure_color = '#d12828';
 
-% % Parallel Computing
-% pool = gcp('nocreate');
-% if isempty(pool)
-%     c = parcluster;                         % get the current cluster object
-%     numDesiredWorkers = c.NumWorkers;       % get the maximum number of workers
-%     pool = parpool(numDesiredWorkers);      % start the parallel pool with the maximum number of workers   
-% end
+% Parallel Computing
+pool = gcp('nocreate');
+if isempty(pool)
+    c = parcluster;                         % get the current cluster object
+    numDesiredWorkers = c.NumWorkers;       % get the maximum number of workers
+    pool = parpool(numDesiredWorkers);      % start the parallel pool with the maximum number of workers   
+end
 
 % Define Simulation Options
-sim_id = "combined_10s";
+sim_id = "combined_60s";
 mkdir(strcat("Results/", sim_id));
-sampling_time = 10;                     % seconds
+sampling_time = 60;                     % seconds
 verbose = true;
 misalignment_type = "oscillating";
 state_perturbation_flag = true;
 engine_failure_flag = true;
 include_actuation = false;
 
-% parfor (mc = 1 : MC, pool.NumWorkers)
-for mc = 1 : MC
+parfor (mc = 1 : MC, pool.NumWorkers)
+% for mc = 1 : MC
 
-    % try
+    try
 
         [RHO_LVLH, M_ctrl_DA, M_ctrl, DU, RHOd_LVLH, dist, vel, deltaState, failure_times, misalignments] = parfmain(sampling_time, include_actuation, verbose, misalignment_type, state_perturbation_flag, engine_failure_flag);
 
@@ -79,13 +79,13 @@ for mc = 1 : MC
     
         table(mc, :) = [mc, data(mc).status, deltaState, norm(deltaState(1:3)), norm(deltaState(4:6))];
 
-    % catch
-    % 
-    %     fprintf('Simulation n° %2d was not successful.\n', mc);
-    %     data(mc).status = -1;
-    %     table(mc, :) = [mc, data(mc).status, zeros(1, 8)];
-    % 
-    % end
+    catch
+
+        fprintf('Simulation n° %2d was not successful.\n', mc);
+        data(mc).status = -1;
+        table(mc, :) = [mc, data(mc).status, zeros(1, 8)];
+
+    end
 
 
 end
