ceb04f92f4ef400526192a67aa3d9bd2134538df
Leonardo Russo <leonardo.rxsso@gmail.com>
2024-05-29 09:02:06 +0200

    combined simulations working!

--- Simulations/parbuilder.m
+++ Simulations/parbuilder.m
@@ -12,7 +12,7 @@ addpath('../Data/Materials/')
 addpath('../Data/Ephemeris/')
 
 root_dir = "Results";       % root results folder
-sim_id = "combined_10s";        % specific results identifier
+sim_id = "combined_60s";        % specific results identifier
 
 
 %% Extract the Results
@@ -33,7 +33,7 @@ fprintf('\nFinal Position Error:\nmean = %.6f mm    std = %.6f mm\n\nFinal Veloc
 is_safe = zeros(MC, 1);
 for k = 1 : MC
     
-    min_dist = 7.9;
+    min_dist = 9.8;
     is_safe(k) = checkMultipleCrossings(data(k).dist*data(k).DU*1e3, min_dist);
     if is_safe(k) > 0
         fprintf('Warning: Distance < %1.1fm in %.0f\n', min_dist, k);
@@ -41,7 +41,7 @@ for k = 1 : MC
 
 end
 
-% return
+return
 terminal_traj = figure('name', 'Terminal Chaser Trajectory in LVLH Space', 'WindowState', 'maximized');
 % title('Terminal Chaser LVLH Trajectory')
 for k = 1 : MC
